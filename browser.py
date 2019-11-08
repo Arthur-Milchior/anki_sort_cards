513e2bde719e4611e34475b136aa05ade65ae31d
Arthur Milchior <Arthur@Milchior.fr>
2019-11-08 02:58:42 +0100

    access sort from browser

---
+++ browser.py
@@ -0,0 +1,24 @@
+from anki.hooks import addHook
+from .sched import sortCids
+
+def addToBrowser(fun, text, shortcut=None):
+    """fun -- function taking as argument: the browser
+    text -- what to enter in the menu
+    shortcut
+    """
+    def aux(browser):
+        action = browser.form.menuEdit.addAction(text)
+        action.triggered.connect(lambda: fun(browser))
+    addHook("browser.setupMenus", aux)
+    if shortcut:
+        action.setShortcut(QKeySequence(shortcut))
+
+def sort(self):
+    cids = self.selectedCards()
+    params = self.col.conf.get("special sort", "")
+    (params, ret) = getText("How to sort those cards", self, default=params)
+    if not ret:
+        return
+    self.col.conf["special sort"] = params
+    sortCids(cids, f"[{params}]")
+addToBrowser(fun, "Sort", "Alt+S")
